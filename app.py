d077031481b80317844dafd743f3757cbeabb83e
Yannick Ritz <yannick@ritz.ooo>
2020-03-21 17:46:03 +0100

    change flask instance var to application so beanstalk will work properly

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
 from flask import Flask, render_template
-app = Flask(__name__,
+application = Flask(__name__,
             static_url_path='', 
             static_folder='static',
             template_folder='templates')
 
 
-@app.route("/")
+@application.route("/")
 def index():
   return render_template('index.html')
-
-@app.route("/login")
+  
+@application.route("/login")
 def login():
   return render_template('login.html')
 
 
 if __name__ == "__main__":
-  app.run()
+    application.debug = True
+    application.run()
